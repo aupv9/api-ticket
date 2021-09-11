df12d5bdb0526becfb995434c78567dc27ebaa93
AuPhan <aupv96@gmail.com>
2021-09-11 23:16:34 +0700

    update *

---
+++ Ticket-System.Api/src/main/java/com/apps/config/cache/CacheConfig.java
@@ -0,0 +1,35 @@
+package com.apps.config.cache;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class CacheConfig {
+    @Bean
+    public ApplicationCacheManager initApplicationCacheManager(
+            @Autowired(required = false) CacheManager cacheManager,
+            @Autowired(required = false) RedisTemplate<Object, Object> redisTemplate,
+            @Value("${spring.cache.redis.key-prefix:}") String redisKeyPrefix) {
+        return new ApplicationCacheManager(cacheManager, redisTemplate, redisKeyPrefix);
+    }
+    
+//    @Bean
+//    @ConditionalOnExpression("'${application.cache.redis.healthcheck}'=='true'")
+//    // @ConditionalOnBean(CacheManager.class)
+//    public CacheHealthIndicator initCacheHealthIndicator(CacheManager cacheManager, JedisConnectionFactory jedisConnectionFactory) {
+//
+//        return new CacheHealthIndicator(cacheManager, jedisConnectionFactory);
+//    }
+
+//    @Bean
+//    public LockProvider lockProvider(JedisConnectionFactory jedisConnectionFactory, @Value("${spring.cache.redis.key-prefix:}") String redisKeyPrefix) {
+//        return new RedisLockProvider(jedisConnectionFactory, redisKeyPrefix);
+//    }
+
+}
